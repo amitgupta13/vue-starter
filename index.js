4bf96610f4873da3500e1458ac43e3cd24008594
Amit Gupta <amit.gupta@daffodilsw.com>
2020-09-21 10:43:26 +0530

    nc

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 new Vue({
-	el: '#app',
+  el: "#app",
   data: {
-  	title: 'Hello world!',
+    title: "Hello world!",
     link: "https://google.com",
-    finishedLink: "<a href='https://google.com'>Google</a>"
+    finishedLink: "<a href='https://google.com'>Google</a>",
+    counter: 0,
+    x: 0,
+    y: 0,
+    name: "Max",
+    secondCounter: 0,
+    red: false,
+    color: "green",
+    fontSize: "16",
+  },
+  computed: {
+    output: function () {
+      console.log("Computed");
+      return this.counter > 5 ? "Greater than 5" : "Smaller than 5";
+    },
+    addClasses() {
+      return {
+        blue: !this.red,
+        red: this.red,
+      };
+    },
+    myStyle() {
+      return {
+        color: this.color,
+        "font-size": this.fontSize + "px",
+      };
+    },
+  },
+  watch: {
+    counter: function (value) {
+      console.log({ value });
+      const vm = this;
+      setTimeout(function () {
+        vm.counter = 0;
+      }, 2000);
+    },
   },
   methods: {
-  	sayHello: function () {
-    	this.title = 'Hello';
+    sayHello: function () {
+      this.title = "Hello";
       return this.title;
     },
     changeTitle: function (e) {
       this.title = e.target.value;
-    }
-  }
-})
+    },
+    result: function (step, e) {
+      console.log("Method");
+      return this.counter > 5 ? "Greater than 5" : "Smaller than 5";
+    },
+    updateCoordinates: function (e) {
+      this.x = e.clientX;
+      this.y = e.clientY;
+    },
+    alertMe: function () {
+      alert("Yo");
+    },
+  },
+});
